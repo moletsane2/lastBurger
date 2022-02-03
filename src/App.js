5317bdadc694273f448dc67c48a06e95e8772b2f
moletsane2 <90116842+moletsane2@users.noreply.github.com>
2022-02-03 06:32:43 +0200

    food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
 import logo from './logo.svg';
 import './App.css';
 
+export function Topbread(){
+  const breadstyle ={
+    backgroundColor: "goldenrod",
+    width:"200px",
+    margin:"0 auto",
+    textAlign:"center",
+    bordertopright: "20px 50px",
+    borderTopLeftRadius:"30px",
+    borderTopRightRadius:"30px",
+    marginBottom:"-17px",
+    marginTop:"200px",
+    color:"goldenrod",
+    justifyContent:"center",
+    alignItems:"center",
+
+
+  };
+  return<> 
+  <div style={breadstyle}>
+  <h2>topbread</h2>
+  </div>
+  </>
+}
+
+export function Tomato(){
+  const tom ={
+    backgroundColor: "red",
+    width:"200px",
+    margin:"0 auto",
+    textAlign:"center",
+    marginBottom:"-17px"
+  };
+  return<> 
+   <div style={tom}>
+  <h2>tomato</h2>
+  </div>
+  </>
+}
+
+export function Meat(){
+  const b ={
+    backgroundColor: "saddlebrown",
+    width:"200px",
+    margin:"0 auto",
+    textAlign:"center",
+    borderRadius:"20px",
+    marginTop:"-17px"
+    
+  };
+  return<> 
+   <div style={b}>
+  <h2>meat</h2>
+  </div>
+  </>
+}
+
+export function Lattuce(){
+  const Lutt ={
+    backgroundColor: "green",
+    width:"200px",
+    margin:"0 auto",
+    textAlign:"center",
+    marginTop:"-17px"
+  };
+  return<> 
+   <div style={Lutt}>
+  <h2>Lattuce</h2>
+  </div>
+  </>
+}
+
+export function Lattce(){
+  const Lutt ={
+    backgroundColor: "goldenrod",
+    width:"200px",
+    margin:"0 auto",
+    textAlign:"center",
+    marginTop:"-17px",
+    color:"goldenrod",
+    borderBottomRightRadius:"10px",
+    borderBottomLeftRadius:"10px"
+  };
+  return<> 
+   <div style={Lutt}>
+  <h2>Lattuce</h2>
+  </div>
+  </>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <><><Topbread />
+    </><><Tomato />
+      </><><Meat />
+      </><><Lattuce />
+      <><Lattce/></>
+      </></> 
   );
 }
 
